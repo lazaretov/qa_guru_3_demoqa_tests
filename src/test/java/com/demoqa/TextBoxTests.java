3e644c4012ec1dde4d027ea174be75d7899ab97f
Timofei <lazaretov03@gmail.com>
2022-09-20 22:18:05 +0300

    add name, lastname, email, phone environment

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -19,16 +19,24 @@ public class TextBoxTests {
 
     @Test
     void fillFormTest () {
+        String firstName = "Dude";
+        String lastName = "Random";
+        String email = "random@dude.ai";
+        String phone = "88005553535";
+
         open("/automation-practice-form");
         Selenide.zoom(0.75);
             //Насколько хорошая идея использовать масштаб?
-        $("#firstName").setValue("Dude");
-        $("#lastName").setValue("Random");
-        $("#userEmail").setValue("random@dude.ai");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
         $(".custom-control-label").click();
             /*Не смог найти способ выбрать не первый радио-баттон при таком запросе, как сослаться к
-            <label title for="gender-radio-2" class="custom-control-label">Female</label>*/
-        $("#userNumber").setValue("88005553535");
+            <label title for="gender-radio-2" class="custom-control-label">Female</label> ? */
+        $("#userNumber").setValue(phone);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(5));
+        $(".react-datepicker__year-select").selectOption("1991");
 
 
     }
