966ad7d8f8aadfdef6a21be72bd33c72d1fc95ec
Timofei <lazaretov03@gmail.com>
2022-09-20 15:57:44 +0300

    add TextBoxTests.java BeforAll and open page

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,26 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+public class TextBoxTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void fillFormTest () {
+        open("/automation-practice-form");
+        Selenide.zoom(0.75); //Насколько хорошая идея использовать масштаб?
+
+    }
+}
